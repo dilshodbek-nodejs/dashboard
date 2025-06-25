a32a5ed20c6e25299ef110582eeb47847eaefb52
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-06-25 17:36:14 +0500

    changed localhost to ip server

--- frontend/src/components/BlogList.tsx
+++ frontend/src/components/BlogList.tsx
@@ -10,7 +10,7 @@ interface BlogListProps {
 }
 
 const getImageUrl = (url: string | null) => {
-  const backendBaseUrl = 'http://localhost:4000';
+  const backendBaseUrl = 'http://15.235.141.2:4000';
   if (!url) return 'https://via.placeholder.com/150'; // Fallback image
   return url.startsWith('http') ? url : backendBaseUrl + url;
 };
