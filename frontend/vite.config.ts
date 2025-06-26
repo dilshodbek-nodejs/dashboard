a1cd398ef346449fb5282fa9db590ea1f9046967
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-06-26 11:21:27 +0500

    done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     build: {
         outDir: 'dist',
-        assetsDir: 'assets',
+        assetsDir: '',
         sourcemap: false,
         minify: 'terser',
         terserOptions: {
